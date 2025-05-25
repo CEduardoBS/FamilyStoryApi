a626010af6f861f166fd37ffe7f3e98afebd6266
carlos.eduardo <C.Eduardocontato@outlook.com>
2025-05-25 19:50:25 -0300

    feat:  Mapeamento dos principais endpoints no controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using FamilyStoryApi.Business;
 using FamilyStoryApi.Model;
+using FamilyStoryApi.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,10 +17,47 @@ namespace FamilyStoryApi.Controllers
             _userInfoBusiness = userInfoBusiness;
         }
 
-        [HttpPost]
-        public IActionResult GetUser([FromBody] UserInfo userInfo)
+        [HttpPost("create")]
+        public IActionResult CreteUser([FromBody] UserInfo userInfo)
         {
-            _userInfoBusiness.Create(userInfo);
+            ResultViewModel<UserInfo> result;
+            try
+            {
+                UserInfo userCreated = _userInfoBusiness.Create(userInfo);
+                result = new(data: userCreated);
+            }
+            catch (Exception err)
+            {
+                result = new(error: err.ToString());
+            }
+
+            if (result.Errors.Count > 0)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("id/{id:int}")]
+        public IActionResult GetUser([FromRoute] int id)
+        {
+            _userInfoBusiness.GetById(id);
+            return Ok("Sucesso");
+        }
+
+        [HttpGet("range/skip={skip:int}&take={take:int}")]
+        public IActionResult GetUserByRange([FromRoute] int skip, [FromRoute] int take)
+        {
+            _userInfoBusiness.GetByRange(skip, take);
+
+            return Ok("Sucesso");
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteUser([FromRoute] int id)
+        {
+            _userInfoBusiness.Delete(id);
             return Ok("Sucesso");
         }
     }
